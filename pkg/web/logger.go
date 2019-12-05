75c6d9c79e9b3204f66691c5f0aca7dd54a37c9e
Soundwave <astroshot@outlook.com>
2019-12-05 14:33:46 +0800

    add custom log

---
+++ pkg/web/logger.go
@@ -0,0 +1,67 @@
+package web
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lestrrat/go-file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	Logger gin.HandlerFunc
+)
+
+func init() {
+	Logger = GetLogger()
+}
+
+func GetLogger() gin.HandlerFunc {
+	logClient := logrus.New()
+	// 禁止logrus的输出
+	src, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	logClient.Out = src
+	logClient.SetLevel(logrus.DebugLevel)
+
+	logPath := "logs"
+	logWriter, err := rotatelogs.New(
+		logPath+".%Y-%m-%d-%H-%M.log",
+		rotatelogs.WithLinkName(logPath),
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+		rotatelogs.WithRotationTime(24*time.Hour),
+	)
+
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+	}
+
+	lfHook := lfshook.NewHook(writeMap, &logrus.JSONFormatter{})
+	logClient.AddHook(lfHook)
+
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		end := time.Now()
+		latency := end.Sub(start)
+
+		path := c.Request.URL.Path
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+		logClient.Infof("| %3d | %13v | %15s | %s %s |",
+			statusCode,
+			latency,
+			clientIP,
+			method,
+			path,
+		)
+	}
+}
